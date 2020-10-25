ae2a9dd9b4c635094477a540ded95101e62fd9f4
SteveSapuko <gil.resnick@gmail.com>
2020-10-25 09:05:43 +0200

    Small tweaks

--- sapkol_2.py
+++ sapkol_2.py
@@ -5,13 +5,22 @@ program = open("program.txt", "r")
 
 proLst = program.read().lower().split("\n")
 
-#print(proLst)
-
-if proLst[0] != "#sapkol":
-    print("Invalid File")
-    sys.exit()
 
 
 
+
+def errorPrint(type): #which error to print
+    errorDict = {1:"Invalid file, not a Sapkol file.",
+    2:"invalid command run.",
+    3:"Invalid variable called.",
+    4:"Invalid function called."}
+
+    print("Error " + str(type) + ": " + errorDict[type])
+
+def interpreter():
+    if proLst[0] != "$sapkol":
+        errorPrint(1)
+        sys.exit()
+interpreter()
 
 program.close()
